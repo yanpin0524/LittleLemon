9e646c2f423163bf7f3a5ac8226a43fae3c7e374
Yanpin <a131234568@gmail.com>
2023-12-08 10:06:30 +0800

    rename models fields

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    Name = models.CharField(max_length=255)
-    No_of_guests = models.IntegerField()
-    BookingDate = models.DateTimeField()
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    bookingDate = models.DateTimeField()
 
 class Menu(models.Model):
-    Title = models.CharField(max_length=255)
-    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
