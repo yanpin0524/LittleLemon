32ee5e72a7343f8d20ce09b8e6b5c9fbcf389e41
Yanpin <a131234568@gmail.com>
2023-12-08 11:16:51 +0800

    create Menu and Booking CRUD API

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path("", views.index, name="home"),
+    path("menu/", views.MenuItemsView.as_view()),
+    path("menu/<int:pk>", views.SingleMenuItemView.as_view()),
 ]
